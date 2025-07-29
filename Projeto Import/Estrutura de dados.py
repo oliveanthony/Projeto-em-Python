897eb66dbc66c56482d1626566d1f0fa513c7b18
Anthony Oliveira Carvalho <anthonyol733@gmail.com>
2025-07-28 23:16:42 -0300

    Delete Projeto Import directory

--- Projeto Import/Estrutura de dados.py
+++
@@ -1,5 +0,0 @@
-número = 7 
-se número % 2 == 0
-           resultado = "Par"
-señao:
-           resultado = "Impar"
--- Projeto Import/Idade_20.py
+++
@@ -1,7 +0,0 @@
-idade = 20
-
-if idade < 18:
-    print("É menor de idade")
-    
-else:
-    print("É maior de idade")
